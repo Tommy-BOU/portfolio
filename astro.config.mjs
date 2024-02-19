b5ba5886423e8e894c43db2fe552bf2bd1bf370f
Tommy BOUBAKER <93928115+Tommy-BOU@users.noreply.github.com>
2024-02-19 09:31:45 +0000

    .

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,4 +5,6 @@ import vue from "@astrojs/vue";
 // https://astro.build/config
 export default defineConfig({
   integrations: [vue()],
+  site: 'https://Tommy-BOU.github.io',
+  base: '/portfolio',
 });
